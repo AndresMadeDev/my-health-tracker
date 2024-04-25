5c2e4366381b34c167703c823f47ef062347ed36
Andres Made <andres.made.dev@gmail.com>
2024-04-25 12:42:50 -0400

    created xcode project

---
+++ Health Tracker/Health_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_TrackerApp.swift
+//  Health Tracker
+//
+//  Created by Andres Made on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Health_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
